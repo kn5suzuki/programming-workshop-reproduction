e8c201df6615970f81d416c764709850255d8905
kn5suzuki <suzukikengo@g.ecc.u-tokyo.ac.jp>
2023-06-21 03:02:07 +0900

    sample app

--- README.md
+++ README.md
@@ -6,4 +6,5 @@ npm install --save express
 npm install --save-dev webpack webpack-cli webpack-node-externals webpack-dev-server
 npm install --save-dev @babel/core @babel/preset-env babel-loader
 npm install  --save-dev html-loader html-webpack-plugin
+npm install --save-dev css-loader style-loader
 ```
--- webpack.config.js
+++ webpack.config.js
@@ -1,49 +1,54 @@
-const path = require('path')
-const webpack = require('webpack')
-const nodeExternals = require('webpack-node-externals')
-const HtmlWebPackPlugin = require("html-webpack-plugin")
+const path = require("path");
+const webpack = require("webpack");
+const HtmlWebPackPlugin = require("html-webpack-plugin");
 module.exports = {
   entry: {
-    server: './server.js',
+    main: "./src/index.js",
   },
   output: {
-    path: path.join(__dirname, 'dist'),
-    publicPath: '/',
-    filename: '[name].js'
+    path: path.join(__dirname, "dist"),
+    publicPath: "/",
+    filename: "[name].js",
+    assetModuleFilename: "images/[hash][ext][query]",
   },
-  target: 'node',
-  node: {
-    // Need this when working with express, otherwise the build fails
-    __dirname: false,   // if you don't put this is, __dirname
-    __filename: false,  // and __filename return blank or /
-  },
-  externals: [nodeExternals()], // Need this to avoid error when working with Express
+  target: "web",
+  devtool: "source-map",
   module: {
     rules: [
       {
-        // Transpiles ES6-8 into ES5
         test: /\.js$/,
         exclude: /node_modules/,
-        use: {
-          loader: "babel-loader",
-          options: {
-            presets: ['@babel/preset-env']
-          }
-        }
+        loader: "babel-loader",
+        options: {
+          presets: ["@babel/preset-env"],
+        },
       },
       {
         // Loads the javacript into html template provided.
-        // Entry point is set below in HtmlWebPackPlugin in Plugins 
+        // Entry point is set below in HtmlWebPackPlugin in Plugins
         test: /\.html$/,
-        use: [{loader: "html-loader"}]
-      }
-    ]
+        use: [
+          {
+            loader: "html-loader",
+            //options: { minimize: true }
+          },
+        ],
+      },
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.(png|svg|jpg|gif)$/,
+        type: "asset/resource",
+      },
+    ],
   },
   plugins: [
     new HtmlWebPackPlugin({
-      template: "./index.html",
+      template: "./src/html/index.html",
       filename: "./index.html",
-      excludeChunks: [ 'server' ]
-    })
-  ]
-}
+      excludeChunks: ["server"],
+    }),
+  ],
+};
